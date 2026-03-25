81b6a135dab2566075759775ea9ad74da83578b5
GZY-q <2965753496@qq.com>
2026-03-25 21:16:56 +0800

    debug

--- backend/models.py
+++ backend/models.py
@@ -7,9 +7,9 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, index=True, nullable=True)
-    hashed_password = Column(String)
+    username = Column(String(100), unique=True, index=True)
+    email = Column(String(200), index=True, nullable=True)
+    hashed_password = Column(String(200))
     created_at = Column(DateTime, default=datetime.utcnow)
 
     tasks = relationship("Task", back_populates="owner")
@@ -19,7 +19,7 @@ class Task(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
-    content = Column(String, index=True)
+    content = Column(String(500), index=True)
     quadrant = Column(Integer) # 1, 2, 3, 4
     is_completed = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.utcnow)
